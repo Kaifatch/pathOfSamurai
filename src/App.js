71e6b9141c85db824bd9024b11303a1895cd3c29
Kaifatch <suvorov63ru@ya.ru>
2020-10-22 19:47:41 +0400

    feat: add React.lazy and React.suspense

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-import {withRouter, Route, BrowserRouter} from "react-router-dom";
+import { withRouter, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
+import { connect, Provider } from "react-redux";
+import { compose } from "redux";
+import { initializeApp } from "./redux/app-reducer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-import Login from "./components/Login/Login";
-import {connect, Provider} from "react-redux";
-import {compose} from "redux";
-import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
 import Navbar from "./components/Navbar/Navbar";
 import store from "./redux/redux-store";
+import { withSuspense } from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const Login = React.lazy(() => import('./components/Login/Login'));
 
 class App extends React.Component {
   componentDidMount() {
@@ -20,29 +22,29 @@ class App extends React.Component {
 
   render() {
     if (!this.props.initialized) {
-      return <Preloader/>
+      return <Preloader />
     }
     return (
-        <div className="app-wrapper">
-          <HeaderContainer/>
-          <Navbar/>
-          <div className="app-wrapper-content">
-            <Route path="/dialogs" render={() => <DialogsContainer/>}/>
-            <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
-            <Route path="/users" render={() => <UsersContainer/>}/>
-            <Route path="/login" render={() => <Login/>}/>
-          </div>
+      <div className="app-wrapper">
+        <HeaderContainer />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/dialogs" render={withSuspense(DialogsContainer)} />
+          <Route path="/profile/:userId?" render={withSuspense(ProfileContainer)} />
+          <Route path="/users" render={withSuspense(UsersContainer)} />
+          <Route path="/login" render={withSuspense(Login)} />
         </div>
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
- initialized: state.app.initialized
+  initialized: state.app.initialized
 })
 
 let AppContainer = compose(withRouter,
-  connect(mapStateToProps, {initializeApp}))(App);
+  connect(mapStateToProps, { initializeApp }))(App);
 
 const SamuraiJSApp = (props) => {
   return <BrowserRouter>
